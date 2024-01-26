29c0625bda382ec96b6562d0cc6dd7114ed1d97a
taphuonglinh <taphuonglinh@luvina.net>
2024-01-26 09:51:42 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
                     <Player name='Player 1' symbol='X' isActive={activePlayer === 'X'}/>
                     <Player name='Player 2' symbol='O' isActive={activePlayer === 'O'}/>
                 </ol>
-                <GameBoard activePlayer={activePlayer} onSelectSquare={handleSelectSquare}/>
+                <GameBoard onSelectSquare={handleSelectSquare} activePlayer={activePlayer} />
             </div>
         </main>
     )
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -8,18 +8,92 @@ const initialGameBoard = [
 
 export default function GameBoard({onSelectSquare, activePlayer}) {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    let notification;
 
     function handleSelectSquare(rowIndex, colIndex) {
+        let checkGameBoardFull = 0;
+
         setGameBoard((prevGameBoard) => {
             const updateGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
 
-            if(updateGameBoard[rowIndex][colIndex] === null) {
+            if (updateGameBoard[rowIndex][colIndex] === null) {
                 updateGameBoard[rowIndex][colIndex] = activePlayer;
+                checkGameBoardFull++;
                 onSelectSquare();
             }
             return updateGameBoard;
         });
 
+        const
+        // debugger
+        if(checkWinner(rowIndex,colIndex)) {
+            notification = gameBoard[rowIndex][colIndex] + " is winner!";
+        } else if(checkGameBoardFull === 6 && !checkWinner(rowIndex,colIndex)) {
+            notification = "Hòa rồi chơi ván mới đê!";
+        }
+        console.log(notification);
+    }
+
+    function checkWinner(rowIndex, colIndex) {
+debugger
+        if (((rowIndex === 1) && (colIndex === 1)) || ((rowIndex !== 1) && (colIndex !== 1))) {
+            if (checkHorizontalLine(rowIndex, colIndex) || checkVerticalLine(rowIndex, colIndex) || checkDiagonalLine(rowIndex, colIndex)) {
+                return true;
+            } else return false;
+        } else {
+            if (checkHorizontalLine(rowIndex, colIndex) || checkVerticalLine(rowIndex, colIndex)) {
+                return true;
+            } else return false;
+        }
+
+    }
+
+    // check duong ngang
+    function checkHorizontalLine(rowIndex, colIndex) {
+debugger
+        let count = 0;
+        for (let i = 0; i < 3; i++) {
+            if (gameBoard[rowIndex][colIndex] === gameBoard[rowIndex][i]) {
+                count++;
+            }
+        }
+        if (count === 3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // check duong doc
+    function checkVerticalLine(rowIndex, colIndex) {
+        debugger
+        let count = 0;
+        for (let i = 0; i < 3; i++) {
+            if (gameBoard[rowIndex][colIndex] === gameBoard[i][colIndex]) {
+                count++;
+            }
+        }
+        if (count === 3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // check duong cheo
+    function checkDiagonalLine(rowIndex, colIndex) {
+        debugger
+        let count = 0;
+            if ((gameBoard[rowIndex][colIndex] === gameBoard[0][0] && gameBoard[rowIndex][colIndex] === gameBoard[1][1] && gameBoard[rowIndex][colIndex] === gameBoard[2][2])
+            || (gameBoard[rowIndex][colIndex] === gameBoard[0][2] && gameBoard[rowIndex][colIndex] === gameBoard[1][1] && gameBoard[rowIndex][colIndex] === gameBoard[2][0])) {
+                count++;
+            }
+
+        if (count === 3) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     return (
@@ -27,10 +101,13 @@ export default function GameBoard({onSelectSquare, activePlayer}) {
             {gameBoard.map((row, indexRow) => <li key={indexRow}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                        <button onClick={() => handleSelectSquare(indexRow,colIndex)}>{playerSymbol}</button>
+                        <button onClick={() => handleSelectSquare(indexRow, colIndex)}>{playerSymbol}</button>
                     </li>)}
                 </ol>
-            </li>)}
+            </li>
+            )}
+
+
             {/*<li>*/}
             {/*    <ol>*/}
             {/*        <li>{initialGameBoard[0][0]}</li>*/}
