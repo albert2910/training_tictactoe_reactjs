ca807b6edb897ac4cd05d9b05b78abb8f88ff5eb
taphuonglinh <taphuonglinh@luvina.net>
2024-01-29 09:52:22 +0700

    done except check win

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import {useState} from "react";
-
+import Log from "./components/Log.jsx";
+function deriveActivePlayer(turns) {
+    let currentPlayer = "X";
+    if (turns.length > 0 && turns[0].player === "X") {
+        currentPlayer = "O";
+    }
+    return currentPlayer;
+}
 function App() {
 
-    const [activePlayer, setActivePlayer] = useState('X');
+    const [turn, setTurn] = useState([]);
+    const activePlayer = deriveActivePlayer(turn);
+    function handleSelectSquare(rowIndex, colIndex) {
 
-    function handleSelectSquare() {
-        setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X');
+        setTurn((preTurn) => {
+            const currentPlayer = deriveActivePlayer(preTurn);
+
+            const updatedTurn = [{square: {row: rowIndex, col: colIndex}, player: currentPlayer}, ...preTurn];
+            return updatedTurn;
+
+        })
     }
 
     return (
         <main>
             <div id="game-container">
-                <ol id="players" >
+                <ol id="players">
                     <Player name='Player 1' symbol='X' isActive={activePlayer === 'X'}/>
                     <Player name='Player 2' symbol='O' isActive={activePlayer === 'O'}/>
                 </ol>
-                <GameBoard onSelectSquare={handleSelectSquare} activePlayer={activePlayer} />
+                <GameBoard onSelectSquare={handleSelectSquare} activePlayer={activePlayer} turns={turn}/>
             </div>
+            <Log turns={turn}/>
         </main>
     )
 
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,112 +1,110 @@
-import {useState} from "react";
-
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ]
 
-export default function GameBoard({onSelectSquare, activePlayer}) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
-    let notification;
+export default function GameBoard({onSelectSquare, turns}) {
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        let checkGameBoardFull = 0;
+    let gameBoard = initialGameBoard;
 
-        setGameBoard((prevGameBoard) => {
-            const updateGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-
-            if (updateGameBoard[rowIndex][colIndex] === null) {
-                updateGameBoard[rowIndex][colIndex] = activePlayer;
-                checkGameBoardFull++;
-                onSelectSquare();
-            }
-            return updateGameBoard;
-        });
-
-        const
-        // debugger
-        if(checkWinner(rowIndex,colIndex)) {
-            notification = gameBoard[rowIndex][colIndex] + " is winner!";
-        } else if(checkGameBoardFull === 6 && !checkWinner(rowIndex,colIndex)) {
-            notification = "Hòa rồi chơi ván mới đê!";
-        }
-        console.log(notification);
+    for (const turn of turns) {
+        const {square, player} = turn;
+        const {row, col} = square;
+        gameBoard[row][col] = player;
     }
 
-    function checkWinner(rowIndex, colIndex) {
-debugger
-        if (((rowIndex === 1) && (colIndex === 1)) || ((rowIndex !== 1) && (colIndex !== 1))) {
-            if (checkHorizontalLine(rowIndex, colIndex) || checkVerticalLine(rowIndex, colIndex) || checkDiagonalLine(rowIndex, colIndex)) {
-                return true;
-            } else return false;
-        } else {
-            if (checkHorizontalLine(rowIndex, colIndex) || checkVerticalLine(rowIndex, colIndex)) {
-                return true;
-            } else return false;
-        }
-
-    }
-
-    // check duong ngang
-    function checkHorizontalLine(rowIndex, colIndex) {
-debugger
-        let count = 0;
-        for (let i = 0; i < 3; i++) {
-            if (gameBoard[rowIndex][colIndex] === gameBoard[rowIndex][i]) {
-                count++;
-            }
-        }
-        if (count === 3) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    // check duong doc
-    function checkVerticalLine(rowIndex, colIndex) {
-        debugger
-        let count = 0;
-        for (let i = 0; i < 3; i++) {
-            if (gameBoard[rowIndex][colIndex] === gameBoard[i][colIndex]) {
-                count++;
-            }
-        }
-        if (count === 3) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    // check duong cheo
-    function checkDiagonalLine(rowIndex, colIndex) {
-        debugger
-        let count = 0;
-            if ((gameBoard[rowIndex][colIndex] === gameBoard[0][0] && gameBoard[rowIndex][colIndex] === gameBoard[1][1] && gameBoard[rowIndex][colIndex] === gameBoard[2][2])
-            || (gameBoard[rowIndex][colIndex] === gameBoard[0][2] && gameBoard[rowIndex][colIndex] === gameBoard[1][1] && gameBoard[rowIndex][colIndex] === gameBoard[2][0])) {
-                count++;
-            }
-
-        if (count === 3) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    // let notification;
+    //
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //
+    //
+    //     setGameBoard((prevGameBoard) => {
+    //         const updateGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //
+    //         if (updateGameBoard[rowIndex][colIndex] === null) {
+    //             updateGameBoard[rowIndex][colIndex] = activePlayer;
+    //             checkGameBoardFull++;
+    //             onSelectSquare();
+    //         }
+    //         return updateGameBoard;
+    //     });
+    //     // debugger
+    //
+    // }
+    // useEffect((rowIndex, colIndex) => {
+    //     notify(rowIndex,colIndex)
+    // }, [gameBoard]);
+    // function notify(rowIndex, colIndex) {
+    //     let checkGameBoardFull = 0;
+    //     if (checkWinner(rowIndex, colIndex)) {
+    //         notification = gameBoard[rowIndex][colIndex] + " is winner!";
+    //     } else if (checkGameBoardFull === 6 && !checkWinner(rowIndex, colIndex)) {
+    //         notification = "Hòa rồi chơi ván mới đê!";
+    //     }
+    //     console.log(notification);
+    // }
+    //
+    // function checkWinner(rowIndex, colIndex) {
+    //     // debugger
+    //     if (((rowIndex === 1) && (colIndex === 1)) || ((rowIndex !== 1) && (colIndex !== 1))) {
+    //         notification = gameBoard[rowIndex][colIndex] + " is winner!";
+    //         return !!(checkHorizontalLine(rowIndex, colIndex) || checkVerticalLine(rowIndex, colIndex) || checkDiagonalLine(rowIndex, colIndex));
+    //     } else {
+    //         notification = "Hòa rồi chơi ván mới đê!";
+    //         return !!(checkHorizontalLine(rowIndex, colIndex) || checkVerticalLine(rowIndex, colIndex));
+    //     }
+    //
+    // }
+    //
+    // // check duong ngang
+    // function checkHorizontalLine(rowIndex, colIndex) {
+    //     // debugger
+    //     let count = 0;
+    //     for (let i = 0; i < 3; i++) {
+    //         if (gameBoard[rowIndex][colIndex] === gameBoard[rowIndex][i]) {
+    //             count++;
+    //         }
+    //     }
+    //     return count === 3;
+    // }
+    //
+    // // check duong doc
+    // function checkVerticalLine(rowIndex, colIndex) {
+    //     // debugger
+    //     let count = 0;
+    //     for (let i = 0; i < 3; i++) {
+    //         if (gameBoard[rowIndex][colIndex] === gameBoard[i][colIndex]) {
+    //             count++;
+    //         }
+    //     }
+    //     return count === 3;
+    // }
+    //
+    // // check duong cheo
+    // function checkDiagonalLine(rowIndex, colIndex) {
+    //     // debugger
+    //     let count = 0;
+    //     if ((gameBoard[rowIndex][colIndex] === gameBoard[0][0] && gameBoard[rowIndex][colIndex] === gameBoard[1][1] && gameBoard[rowIndex][colIndex] === gameBoard[2][2])
+    //         || (gameBoard[rowIndex][colIndex] === gameBoard[0][2] && gameBoard[rowIndex][colIndex] === gameBoard[1][1] && gameBoard[rowIndex][colIndex] === gameBoard[2][0])) {
+    //         count++;
+    //     }
+    //
+    //     return count === 3;
+    // }
 
     return (
         <ol id="game-board">
             {gameBoard.map((row, indexRow) => <li key={indexRow}>
-                <ol>
-                    {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                        <button onClick={() => handleSelectSquare(indexRow, colIndex)}>{playerSymbol}</button>
-                    </li>)}
-                </ol>
-            </li>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => <li key={colIndex}>
+                            <button onClick={() => onSelectSquare(indexRow, colIndex)}
+                                    disabled={playerSymbol !== null}>{playerSymbol}</button>
+                        </li>)}
+                    </ol>
+                </li>
             )}
-
 
             {/*<li>*/}
             {/*    <ol>*/}
